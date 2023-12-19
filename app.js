761cd1289c808471c92b9004b44f5f79728996e8
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-19 12:53:19 +0000

    [add] #4 Chapter4_app.js コードの説明を追加

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// 「bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい」という意味（全体）
+// 5行目
+// body:selector,html:method,<h1>Hello jQuery!!</h1>:parameter
 $(document).ready(function () {
   $('body').html('<h1>Hello jQuery!!</h1>');
 });
